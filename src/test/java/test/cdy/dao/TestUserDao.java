33c4ab1488f6860d549cfce67e892d15783841ae
wentao_chang <changwenwen@126.com>
2014-07-23 17:51:54 +0800

    完善测试用例

--- src/test/java/test/cdy/dao/TestUserDao.java
+++ src/test/java/test/cdy/dao/TestUserDao.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,7 +19,7 @@ public class TestUserDao extends AbstractTestCase
     private UserDao userDao;
     
     @Test
-    public void testMyDao() throws Exception
+    public void insertList() throws Exception
     {
         List<User> userlist=new ArrayList<User>();
         User user=new User();
@@ -34,6 +35,37 @@ public class TestUserDao extends AbstractTestCase
         userlist.add((User)user.clone());
         
         userDao.insert(userlist);
-
+        for(User u:userlist){
+            Assert.assertNotNull(u.getUserId());
+        }
+    }
+    @Test
+    public void insert() throws Exception
+    {
+        User user=new User();
+        user.setCredit(100);
+        user.setLastIp("192.101.19.122");
+        user.setLastVisit(new Date());
+        user.setLocked(1);
+        user.setPassword("pass");
+        user.setUserName("name");
+        user.setUserType(1);
+        userDao.insert(user);
+        Assert.assertNotNull(user.getUserId());
+    }
+    @Test
+    public void getUserByUserName() throws Exception{
+        User user=new User();
+        user.setCredit(100);
+        user.setLastIp("192.101.19.122");
+        user.setLastVisit(new Date());
+        user.setLocked(1);
+        user.setPassword("pass");
+        user.setUserName("name");
+        user.setUserType(1);
+        userDao.insert(user);
+        
+        User u=userDao.getUserByUserName("name");
+        Assert.assertNotNull(u);
     }
 }
